bbe4df0815cfcaf9ae221429cd4b8ecb377556e5
杨伟玉_4252 <31227502+ywy0318@users.noreply.github.com>
2019-06-21 09:26:30 +0800

    Add files via upload

---
+++ java/data_structure/src/data_structure/test001.java
@@ -0,0 +1,11 @@
+package data_structure;
+
+public class test001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+}
